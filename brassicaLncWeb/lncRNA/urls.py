bf12f2d6e4ee6f74e60f01d6fbfe152e70c54f5a
bluewhale <bluewhale.f@gmail.com>
2022-04-06 19:04:48 +0430

    :)

--- brassicaLncWeb/lncRNA/urls.py
+++ brassicaLncWeb/lncRNA/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('create/lnc/', createLNC.as_view(), name='createLNC'),
     path('create/gtf/', createGTF.as_view(), name='createGTF'),
     path('transcripts/', transcripts.as_view(), name='transcripts'),
+    path("chemical/",chemical.as_view(),name="chemical")
 ]
