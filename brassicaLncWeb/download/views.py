c329077466278f98beecdd87c73c6fcb27667f10
Aliz-f <fad127alireza2@gmail.com>
2022-05-08 18:39:44 +0430

    add download for 3 others group

--- brassicaLncWeb/download/views.py
+++ brassicaLncWeb/download/views.py
@@ -109,6 +109,15 @@ class downloadFile(APIView):
                 gp(chemical):
                     chemical-lnc-fpkm => chemical_fpkm.txt
                     chemical-lnc-db => BrassIcaLnc_Tabledb_Chemical_db.tsv
+                gp(genetics):
+                    genetics-lnc-fpkm => genetics_fpkm.txt
+                    genetics-lnc-db => BrassIcaLnc_Tabledb_Genetics_db.tsv
+                gp(developmental):
+                    developmental-lnc-fpkm => developmental_fpkm.txt
+                    developmental-lnc-db => BrassIcaLnc_Tabledb_Developmental_db.tsv
+                gp(abiotic):
+                    abiotic-lnc-fpkm => abiotic_fpkm.txt
+                    abiotic-lnc-db => BrassIcaLnc_Tabledb_Abiotic_db.tsv
             '''
             file=request.GET.get('file',None)
             assert file,'files query param not found'
@@ -121,8 +130,15 @@ class downloadFile(APIView):
                 "database-lnc":f"{os_path}/BrassicaLnc_Final_lncRAN_Table_Database.tsv",
                 "fasta-lnc" : f"{os_path}/Final_lncRNA_rename_V2.fa",
                 "gtf-lnc":f"{os_path}/lncRNA.gtf",
+                "abiotic-lnc-fpkm":f"{os_path}/abiotic/abiotic_fpkm.txt",
+                "abiotic-lnc-db":f"{os_path}/abiotic/BrassIcaLnc_Tabledb_Abiotic_db.tsv",
                 "chemical-lnc-fpkm":f"{os_path}/chemical/chemical_fpkm.txt",
                 "chemical-lnc-db":f"{os_path}/chemical/BrassIcaLnc_Tabledb_Chemical_db.tsv",
+                "developmental-lnc-fpkm":f"{os_path}/developmental/developmental_fpkm.txt",
+                "developmental-lnc-db":f"{os_path}/developmental/BrassIcaLnc_Tabledb_Developmental_db.tsv",
+                "genetics-lnc-fpkm":f"{os_path}/genetics/genetics_fpkm.txt",
+                "genetics-lnc-db":f"{os_path}/genetics/BrassIcaLnc_Tabledb_Genetics_db.tsv",
+
             }
 
             filePath = filesTemplate[file]
