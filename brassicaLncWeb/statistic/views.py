ce962b780a8bb475fc1ac841fe8b488a0e727165
Aliz-f <fad127alireza2@gmail.com>
2022-06-21 16:51:36 +0430

    add orderd to statistic

--- brassicaLncWeb/statistic/views.py
+++ brassicaLncWeb/statistic/views.py
@@ -5,12 +5,18 @@ from rest_framework.pagination import PageNumberPagination
 
 from .serializer import * 
 from .models import *
+from collections import OrderedDict
 
 # Create your views here.
 class CsrfExemptSessionAuthentication(authentication.SessionAuthentication):
     def enforce_csrf(self, request):
         return
 
+
+def ordered(d, desired_key_order):
+    return OrderedDict([(key, d[key]) for key in desired_key_order])
+
+toplevel_desired_key_order = ("Potential novel transripts (Class codes: i, u, x, o, e)", "Transcripts with length > 200 bp and < 15 kb", "Transcripts with FPKM > 0.5 in at least 495 samples","Transcripts after filter out tRNAs and rRNAs", "Noncoding transcripts predicted by CPC2", "LncRNAs predicted by PLncPRO, FEElnc, and CREMA", "Transcripts with no significant hit against UniProt, Pfam, and Rfam.", "Reliably expressed lncRNAs")
 class plotStatistic(APIView):
     permission_classes = (permissions.AllowAny,)
     authentication_classes = (
@@ -26,6 +32,7 @@ class plotStatistic(APIView):
             subdivisionLncRNAsSer=relationshipBetween_Chr_Gene_LncRNASerializer(subdivisionLncRNAs, many=True)
             data = {}
             data['relationship'] = relationshipSer.data
+            filtrationStepsSer.data[0]['data'] =ordered(filtrationStepsSer.data[0]['data'], toplevel_desired_key_order)
             data['filtrationSteps'] = filtrationStepsSer.data
             data['subdivisionLncRNAs'] = subdivisionLncRNAsSer.data
             return Response(data, status=status.HTTP_200_OK)
