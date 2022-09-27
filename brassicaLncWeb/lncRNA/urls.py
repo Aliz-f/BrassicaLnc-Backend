170ccad7fb9d0050947238b06871cc38ac3b6bb9
Aliz-f <fad127alireza2@gmail.com>
2022-09-27 19:32:22 +0330

    chore: :fire: remove api for create add data to database

--- brassicaLncWeb/lncRNA/urls.py
+++ brassicaLncWeb/lncRNA/urls.py
@@ -25,21 +25,10 @@ urlpatterns = [
     #smallRNA
     path("small_rna_target/", GetSmallRnaTarget.as_view(),name="small_rna"),
     path("small_rna_target/ids/", GetSmallRnaTargetIds.as_view(),name="small_rna_ids"),
-    # path("small_rna_target/db/", CreateSmallRNATraget.as_view(),name="small_rna_db"),
     #premiRNA
     path("premi_rna/", GetPremiRna.as_view(),name="premi_rna"),
     path("premi_rna/ids/", GetPremiRnaIds.as_view(),name="premi_rna_ids"),
-    # path("premi_rna/db/", CreatePremiRNA.as_view(),name="premi_rna_db"),
     #etms
     path("etms/", GetEtms.as_view(),name="etms"),
-    # path("etms/db/", CreateEtms.as_view(),name="etms_db"),
-    #* create group database (not public)
-    # path("create/chemicaldb/",create_chemical_db.as_view(),name="createChemical"),
-    # path("create/abioticdb/",create_abiotic_db.as_view(),name="createAbiotic"),
-    # path("create/geneticsdb/",create_genetics_db.as_view(),name="createGenetics"),
-    # path("create/developmentaldb/",create_developmental_db.as_view(),name="createDevelopmental"),
-    # path("create/bioticdb/",create_biotic_db.as_view(),name="createBiotic"),
-    #* create lnc and gtf database (not public)
-    # path('create/lnc/', createLNC.as_view(), name='createLNC'),
-    # path('create/gtf/', createGTF.as_view(), name='createGTF'),
+
 ]
